2cebef19971b62dff1254f4f22345850fa58e2ba
Enrique Narciso <46304338+e-narciso@users.noreply.github.com>
2019-09-25 15:26:10 -0400

    not working yet

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+
+const express = require('express');
+const router  = express.Router();
+const nodemailer = require('nodemailer');
+
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.post('/send-email', (req, res, next) => {
+  let { email, subject, message } = req.body;
+  res.render('message', { email, subject, message })
+});
+
+router.post('/send-email', (req, res, next) => {
+  let { email, subject, message } = req.body;
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.MAILUSER,
+      pass: process.env.MAILPASS,
+    }
+  });
+  transporter.sendMail({
+    from: '"My Awesome Project 👻" <myawesome@project.com>',
+    to: email, 
+    subject: subject, 
+    text: message,
+    html: `<b>${message}</b>`
+  })
+  .then(info => res.render('message', {email, subject, message, info}))
+  .catch(error => console.log(error));
+});
+
+module.exports = router;
